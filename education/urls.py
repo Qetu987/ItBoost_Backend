07501f8545755562ae372cdbf418e001c0586230
Ruslan <sichkaryk39@gmail.com>
2024-08-28 12:23:50 +0300

    fix

--- education/urls.py
+++ education/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('lessons/<int:lesson_id>/theme/', LessonThemeView.as_view(), name='lesson-theme'),
     path('lessons/<int:lesson_id>/check/<int:student_id>/', AttendanceUserCheckView.as_view(), name='attendance-user-check'),
     path('lessons/<int:lesson_id>/mark/<int:student_id>/', AttendanceMarkCheckView.as_view(), name='attendance-mark'),
-    path('homework/set', HomeworksSetView.as_view(), name='homework-set'),
+    path('homework/set/', HomeworksSetView.as_view(), name='homework-set'),
     path('homework/to_check/', HomeworksToCheckView.as_view(), name='homework-to-check')
 ]
